683a85cb041dc0a1b3ae95096db20c204d0a9182
sandymak <31165505+sandymak@users.noreply.github.com>
2023-12-14 21:50:15 +0000

    fix console.logs

--- leetcode/addLarge.js
+++ leetcode/addLarge.js
@@ -10,79 +10,79 @@ In order to solve this problem, I will treat each number is a string and manipul
 
 */
 
-// function addLarge(first, second) {
-//   const longerString = first.length > second.length ? first : second;
-//   const shorterString = first.length > second.length ? second : first;
-//   console.log('first', first, "second", second)
+function addLarge(first, second) {
+  const longerString = first.length > second.length ? first : second;
+  const shorterString = first.length > second.length ? second : first;
+  console.log('first', first, "second", second)
 
-//   let result = null;
-//   let carryover = 0;
-//   let longerStringIndex = longerString.length - 1;
-//   let shorterStringIndex = shorterString.length - 1;
+  let result = '';
+  let carryover = 0;
+  let longerStringIndex = longerString.length - 1;
+  let shorterStringIndex = shorterString.length - 1;
 
-//   while (longerStringIndex >=0) {
-//     console.log('first', longerString[longerStringIndex]);
-//     console.log('seconf', shorterString[shorterStringIndex]);
-//     const firstDigit = Number(longerString[longerStringIndex]) || 0;
-//     const secondDigit = Number(shorterString[shorterStringIndex]) || 0;
-//     const sum = firstDigit + secondDigit + carryover;
-//     console.log('firstDigit', firstDigit);
-//     console.log('secondDigit', secondDigit);
-//     console.log('carryover', carryover);
-//     console.log('sum', sum);
-//     console.log('result', result)
-//     if (sum > 9) {
-//       const remainder = sum % 10;
-//       carryover = Math.floor(sum/10);
-//       result = `${remainder}${result === null ? ' ' : result}`;
-//       console.log('remainder????', remainder, "carryover?????", carryover, "result=", result)
-//     } else {
-//       console.log(`sum? ${sum}, result? ${result}`)
-//       result = `${sum}${result === null ? ' ' : result}`;
-//       carryover = 0;
-//     }
-//     longerStringIndex -=1;
-//     shorterStringIndex -=1;
-//   }
+  while (longerStringIndex >=0) {
+    console.log('first', longerString[longerStringIndex]);
+    console.log('seconf', shorterString[shorterStringIndex]);
+    const firstDigit = Number(longerString[longerStringIndex]) || 0;
+    const secondDigit = Number(shorterString[shorterStringIndex]) || 0;
+    const sum = firstDigit + secondDigit + carryover;
+    console.log('firstDigit', firstDigit);
+    console.log('secondDigit', secondDigit);
+    console.log('carryover', carryover);
+    console.log('sum', sum);
+    console.log('result', result)
+    if (sum > 9) {
+      const remainder = sum % 10;
+      carryover = Math.floor(sum/10);
+      result = `${remainder}${result}`;
+      console.log('remainder????', remainder, "carryover?????", carryover, "result=", result)
+    } else {
+      console.log(`sum? ${sum}, result? ${result}`)
+      result = `${sum}${result}`;
+      carryover = 0;
+    }
+    longerStringIndex -=1;
+    shorterStringIndex -=1;
+  }
 
 
 
-//   console.log(`BEFOREEEE ${first} + ${second} = ${result}`);
+  console.log(`BEFOREEEE ${first} + ${second} = ${result}`);
 
-//   if (carryover > 0) result = `${carryover}${result}`;
+  if (carryover > 0) result = `${carryover}${result}`;
 
-//   console.log(`${first} + ${second} = ${result}`);
-//   return result;
-// }
+  console.log(`${first} + ${second} = ${result}`);
+  return result;
+}
 
 
 /* 
 Super elegant, array solution to a stack problem... this is glorious!
 
 */
-function addLarge (first, second) {
-  const firstToArray = first.split('');
-  const secondToArray = second.split('');
-  let carryover = 0;
-  let result = '';
+// function addLarge (first, second) {
+//   const firstToArray = first.split('');
+//   const secondToArray = second.split('');
+//   let carryover = 0;
+//   let result = '';
 
-  while (firstToArray.length || secondToArray.length || carryover) {
-    const firstDigit = Number(firstToArray.pop()) || 0;
-    const secondDigit = Number(secondToArray.pop()) || 0;
-    const sum = firstDigit + secondDigit + carryover;
+//   while (firstToArray.length || secondToArray.length || carryover) {
+//     const firstDigit = Number(firstToArray.pop()) || 0;
+//     const secondDigit = Number(secondToArray.pop()) || 0;
+//     const sum = firstDigit + secondDigit + carryover;
 
-    if (sum > 9) {
-      const remainder = sum % 10;
-      carryover = Math.floor(sum/10);
-      result = `${remainder}${result}`;
-    } else {
-      result = `${sum}${result}`
-      carryover = 0
-    }
-  }
-    console.log(`${first} + ${second} = ${result}`);
-  return result;
-}
+//     if (sum > 9) {
+//       const remainder = sum % 10;
+//       carryover = Math.floor(sum/10);
+//       result = `${remainder}${result}`;
+//     } else {
+//       result = `${sum}${result}`
+//       carryover = 0
+//     }
+//   }
+//     console.log(`${first} + ${second} = ${result}`);
+//   return result;
+// }
 
 
 module.exports = {addLarge}
