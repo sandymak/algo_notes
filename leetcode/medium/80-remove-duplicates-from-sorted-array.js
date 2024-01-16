8fca9b0028132d70378960f8c199345705634b10
sandymak <31165505+sandymak@users.noreply.github.com>
2024-01-16 19:27:53 +0000

    solve remove duplicates problem

--- leetcode/medium/80-remove-duplicates-from-sorted-array.js
+++ leetcode/medium/80-remove-duplicates-from-sorted-array.js
@@ -3,51 +3,27 @@
  * @return {number}
  */
 var removeDuplicates = function(nums) {
-  if (nums.length < 2) return nums;
+    let l = 0 
+    let r = 0
 
-  let l = 0;
-  let m = 1;
-  let r = 2
-
-  while (r < nums.length) {
-    const lVal = nums[l];
-    const mVal = nums[m];
-    const rVal = nums[r];
-
-    console.log('lVal=', lVal, "mVal=", mVal, "rVal====", rVal)
-
-    if (mVal === null) {
-      nums = swap(m,r, nums)
-      r += 1;
-      continue;
-    } else if (lVal === mVal) {
-      if (mVal === rVal) {
-        m[r] = null;
-        r += 1
-      } else {
-        nums = swap(m +1, r, nums);
-        l = m + 1;
-        m = m +2;
-        r += 1;
-      }
-    } else if (lVal !== mVal) {
-      l += 1;
-      m += 1;
+    while(r<nums.length){
+        let count = 1
+        // count the duplicates in a set & find the end of a set
+        while(r+1<nums.length && nums[r] === nums[r+1]){
+            r+=1
+            count+=1
+        }
+        // build the number of duplicates in the set via the left pointer
+        // left pointer is responsible for finding where a new set should begin
+        for(let i = 0; i<Math.min(2,count);i++){
+            nums[l] = nums[r]
+            l+=1
+        }
+        // move r to a new set of numbers
+        r+=1
     }
-
-  }
-
-  return nums;
+    return l
 
 };
-
-function swap(first, second, nums) {
-  const temp = nums[first];
-  nums[first] = nums[second];
-  nums[second] = temp;
-  return nums;
-}
-
-console.log('removeDFuplicats', removeDuplicates([1,2,2,3,3,3,4]))
 
 module.exports = {removeDuplicates};
